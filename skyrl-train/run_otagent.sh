e8b861e3a9c28acb09a99a72e1ef1f5b081d571c
Xinyu Jiang <xinyuj2@andrew.cmu.edu>
2025-12-31 19:05:27 -0500

    Update run_otagent.sh for new paths and parameters

--- skyrl-train/run_otagent.sh
+++ skyrl-train/run_otagent.sh
@@ -1,18 +1,17 @@
 # My key
-export DAYTONA_API_KEY=YOUR_KEY_HERE
-export WANDB_API_KEY=YOUR_KEY_HERE
+export CUDA_VISIBLE_DEVICES=0,1
 
 # Got after hf download open-thoughts/OpenThoughts-Agent-v1-RL --repo-type=dataset
 # cd into the downloaded folder, say /path/to/.cache/huggingface/hub/datasets--open-thoughts--OpenThoughts-Agent-v1-RL/snapshots/hash_code
 # python extract_parquet_tasks.py tasks_new.parquet ./extracted_tasks
-TRAIN_DATA="['/home/ec2-user/.cache/huggingface/hub/datasets--open-thoughts--OpenThoughts-Agent-v1-RL/snapshots/e4636fa481f6a5a540c74340cf69b2d28150978f/extracted_tasks']"
+TRAIN_DATA="['/root/.cache/huggingface/hub/datasets--open-thoughts--OpenThoughts-Agent-v1-RL/snapshots/fe46b463573e7f849e1f317e4dbe9ee7cc668fdd/extracted_tasks']"
 # Got after hf download open-thoughts/OpenThoughts-TB-dev --repo-type=dataset
-EVAL_DATA="['/home/ec2-user/.cache/huggingface/hub/datasets--open-thoughts--OpenThoughts-TB-dev/snapshots/c1df0436e2d58c89f67d552c36cab9172280c5ae']"
+EVAL_DATA="['/root/.cache/huggingface/hub/datasets--open-thoughts--OpenThoughts-TB-dev/snapshots/0d54f719f34dca712c8d6ef0f51df4670a2a287a']"
 
-CHAT_TEMPLATE_PATH="/home/ec2-user/SkyRL/skyrl-train/examples/terminal_bench/qwen3_thinking_acc.jinja2"
-TRIALS_DIR="/home/ec2-user/trials_run"
-CKPTS_DIR="/home/ec2-user/otagent/ckpts"
-EXPORTS_DIR="/home/ec2-user/otagent/exports"
+CHAT_TEMPLATE_PATH="/mnt/data/xinyu/program/slime-tb/SkyRL/skyrl-train/examples/terminal_bench/qwen3_thinking_acc.jinja2"
+TRIALS_DIR="/mnt/data/xinyu/program/slime-tb/trials_run"
+CKPTS_DIR="/mnt/data/xinyu/program/slime-tb/otagent/ckpts"
+EXPORTS_DIR="/mnt/data/xinyu/program/slime-tb/otagent/exports"
 
 # Run SkyRL command
 python -m examples.terminal_bench.entrypoints.main_tbench \
@@ -35,29 +34,29 @@ python -m examples.terminal_bench.entrypoints.main_tbench \
   trainer.strategy=fsdp2 \
   trainer.placement.policy_num_nodes=1 \
   trainer.placement.ref_num_nodes=1 \
-  trainer.placement.policy_num_gpus_per_node=8 \
-  trainer.placement.ref_num_gpus_per_node=8 \
-  generator.num_inference_engines=8 \
+  trainer.placement.policy_num_gpus_per_node=2 \
+  trainer.placement.ref_num_gpus_per_node=2 \
+  generator.num_inference_engines=2 \
   generator.inference_engine_tensor_parallel_size=1 \
   +generator.engine_init_kwargs.custom_chat_template_chat_completion_path=$CHAT_TEMPLATE_PATH \
   trainer.epochs=3 \
-  trainer.eval_batch_size=128 \
+  trainer.eval_batch_size=32 \
   trainer.eval_before_train=true \
   trainer.eval_interval=20 \
   trainer.update_epochs_per_batch=1 \
-  trainer.train_batch_size=64 \
-  trainer.policy_mini_batch_size=64 \
+  trainer.train_batch_size=16 \
+  trainer.policy_mini_batch_size=16 \
   trainer.micro_forward_batch_size_per_gpu=1 \
   trainer.micro_train_batch_size_per_gpu=1 \
   trainer.ckpt_interval=5 \
   trainer.hf_save_interval=5 \
   trainer.max_prompt_length=2048 \
-  generator.sampling_params.max_generate_length=30720 \
+  generator.sampling_params.max_generate_length=8192 \
   trainer.policy.optimizer_config.lr=1.0e-6 \
   trainer.algorithm.use_kl_loss=true \
-  generator.n_samples_per_prompt=8 \
-  generator.eval_n_samples_per_prompt=8 \
-  generator.gpu_memory_utilization=0.8 \
+  generator.n_samples_per_prompt=2 \
+  generator.eval_n_samples_per_prompt=2 \
+  generator.gpu_memory_utilization=0.45 \
   trainer.logger=wandb \
   trainer.project_name=dc-agent \
   trainer.run_name=otagent-rl \
